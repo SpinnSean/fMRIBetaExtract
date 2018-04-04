c5ac6262898c2fb02643a1d2df33f1064065f8b2
Sean Spinney <sean.c.spinney@gmail.com>
2018-04-04 01:49:11 -0400

    Update CreateWorkS.R

--- scripts/CreateWorkS.R
+++ scripts/CreateWorkS.R
@@ -1,10 +1,11 @@
 CreateWorkS <- function(task='') {
-    indir <- '/data/IMAGEN/Functional/extracted_betas'
+    # If no input is given, default will take All tasks and 
+    # produce a combined workspace. 
+    
+    indir <- '/data/IMAGEN/Functional/extracted_betas' # Where the extracted txt files are
     setwd(indir)
     temp <- list.files(pattern= paste0(task,".*.txt$"))
     list2env(lapply(setNames(temp, make.names(gsub("*.txt$", "", temp))),read.csv, sep='\t'), envir = .GlobalEnv)
-    #rm(list=setdiff(ls(), ls(pattern="Midt*")), envir=.GlobalEnv)
-    #save(list=ls(), file= paste0("/data/IMAGEN/Functional/R_Workspaces/", task, '_workspace.RData'), envir=.GlobalEnv)
     task <- ifelse(task == '', 'ALL', task)
     save.image(file= paste0("/data/IMAGEN/Functional/R_Workspaces/", task, '_workspace.RData'))
 }
